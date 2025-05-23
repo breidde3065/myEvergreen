b6ecb551674d91a817f6405d829917b15e6a9673
breidde3065 <breidde3065@mailfence.com>
2025-05-23 10:18:04 +0300

    Create cart.js

---
+++ cart.js
@@ -0,0 +1,35 @@
+const placeorderbtn=document.getElementById('checkout');
+const orderform=document.getElementById('order-form');
+const form=document.getElementById('orderform');
+const confirmmsg=document.getElementById('confirmation');
+
+placeorderbtn.addEventListener('click',()=>{
+    orderform.style.display='block';
+});
+
+orderdetailsform.addEventListener('submit',async(e)=>{
+    e.preventDefault();
+
+    const userDetails={
+        name:document.getElementById('name').value,
+        phone:document.getElementById('phone').value,
+        location:document.getElementById('location').value,
+        city:document.getElementById('city').value,
+        cartItems:JSON.parse(localStorage.getItem('cart') || '[]')
+    };
+    const res=await fetch('https://evergreendb.glitch.me/place-order',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(userDetails)
+    });
+
+    const data=await res.json();
+    if (res.ok){
+        confirmmsg.innerText=data.message;
+        confirmmsg.style.display='block';
+        localStorage.removeItem('cart');
+        orderform.style.display='none';
+    }else{
+        alert(data.message || 'error placing order');
+    }
+});

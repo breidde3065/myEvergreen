d7b9e6b62dfe7c7883bd7b75cc5870b49b78aac1
breidde3065 <breidde3065@mailfence.com>
2025-05-26 22:24:47 +0300

    Update product.js

--- product.js
+++ product.js
@@ -67,12 +67,12 @@ cart=cart.map(item => ({...item,quantity: item.quantity || 1 }));
 
 function cartProduct(product){
 
-const existingItem=cart.findIndex((item)=> item.id===product.id);
-    if(existingItem !==-1){
-             cart[existingItem].quantity += 1;
+const existingItem=cart.find((item)=> item.id===product.id);
+    if(existingItem){
+        existingItem.quantity = (existingItem.quantity || 1) + 1;
     } else{
-        
-            cart.push({...product,quantity:1 });
+             product.quantity = 1;
+            cart.push(product);
     }
     
    
